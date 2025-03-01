a4dd2e25ca9dd95abf0111c7c7ace155e30442c9
nctmrk0802 <168177234+nctmrk0802@users.noreply.github.com>
2025-03-01 08:22:45 +0800

    Update schoolData.js

--- tests/fixtures/schoolData.js
+++ tests/fixtures/schoolData.js
@@ -1,8 +1,8 @@
 export const validSchools = [
   {
-    school_id: 1,
-    name: 'Test School',
-    address: '123 Test St',
-    contact_no: '2-123-4567',
+    school_id: 1001,
+    name: 'University of Santo Tomas',
+    address: 'Espana Blvd, Sampaloc, Manila, 1008 Metro Manila',
+    contact_no: '+6324061611',
   },
 ]
