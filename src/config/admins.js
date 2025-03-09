127d8cc18a1c4ea1e3bd7ca60d20bf0e206a2ae1
nctmrk0802 <168177234+nctmrk0802@users.noreply.github.com>
2025-03-09 14:57:54 +0800

    Update admins.js

--- src/config/admins.js
+++ src/config/admins.js
@@ -3,7 +3,7 @@ export const adminsList = [
     first_name: 'System',
     last_name: 'Administrator',
     email: 'admin@aralkademy.com', // Change this to your real email to test the forgot password feature
-    password: process.env.ADMIN_PASSWORD,
+    password: process.env.ADMIN_PASSWORD || '',
     role: 'admin',
     is_verified: true,
   },
